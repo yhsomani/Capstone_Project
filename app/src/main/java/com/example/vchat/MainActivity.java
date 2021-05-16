9b1bf93ea8e905e8058152d15b5354c6b4b3e27e
Yash Somani <57716157+yhsomani@users.noreply.github.com>
2021-05-16 23:11:20 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vchat/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.vchat;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(MainActivity.this, Login.class));
+            }
+        },3000);
+        getSupportActionBar().hide();
+
+    }
+}
